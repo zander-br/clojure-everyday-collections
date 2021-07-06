25888c20c7cfc57d4d197717277645a13fe78158
Anderson Santos <anderson.santos@lambda3.com.br>
2021-07-06 17:33:42 -0300

    feat: first, second, rest, count, nth and take

--- src/loja/aula4.clj
+++ src/loja/aula4.clj
@@ -15,8 +15,25 @@
 
 (defn resumo-por-usuario-ordenado [pedidos]
   (->> pedidos
-       (sort-by :preco-total)))
+       l.logic/resumo-por-usuario
+       (sort-by :preco-total)
+       reverse))
 
 (let [pedidos (l.db/todos-os-pedidos)
       resumo (resumo-por-usuario-ordenado pedidos)]
-  (println "Resumo" resumo))
+  (println "Resumo" resumo)
+  (println "Primeiro" (first resumo))
+  (println "Segundo" (second resumo))
+  (println "Resto" (rest resumo))
+  (println "Total" (count resumo))
+  (println "Class" (class resumo))
+  (println "nth 1" (nth resumo 1))
+  (println "take" (take 2 resumo)))
+
+(defn top-2 [resumo]
+  (take 2 resumo))
+
+(let [pedidos (l.db/todos-os-pedidos)
+      resumo (resumo-por-usuario-ordenado pedidos)]
+  (println "Resumo" resumo)
+  (println "Top 2" (top-2 resumo)))
